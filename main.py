740af72e4f81f06db859d236e8f814480e38bf4d
chungchung <hangjastar@gmail.com>
2021-01-19 13:40:50 +0000

    입출력 문제

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-n, k = map(int, input().split())
-weight = [0]
-value = [0]
+n = int(input())
+m = int(input())
 
-for _ in range(n):
-  w, v = map(int, input().split())
-  weight.append(w)
-  value.append(v)
-
-dp = [[0 for _ in range(k+1)] for _ in range(n+1)]
-
-for w in range(1, n+1):
-  for i in range(1, k+1):
-    if i >= weight[w]:
-      dp[w][i] = max(dp[w-1][i], dp[w-1][i-weight[w]] + value[w])
-    else:
-      dp[w][i] = dp[w-1][i]
-      
-print(dp[n][k])
-
+print(n+m)
