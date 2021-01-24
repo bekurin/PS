b17ec1bec44546070930162d7284587fe3d9ee4c
hangjastar <hangjastar@gmail.com>
2021-01-24 15:07:22 +0000

    백준 문제풀이

--- main.py
+++ main.py
@@ -1,20 +1,5 @@
-def solution(N, stages):
-  answer = []
-  length = len(stages)
+t = int(input())
 
-  for i in range(1, N + 1):
-    count = stages.count(i)
-
-    if length == 0:
-      fail = 0
-    else:
-      fail = count / length
-    answer.append((i,fail))
-    length -= count 
-  answer = sorted(answer, key = lambda x: x[1], reverse = True)
-  answer = [i[0] for i in answer]
-  return answer
-
-n = 5
-stages = [2,1,2,6,2,4,3,3]
-print(solution(n, stages))
+for _ in range(t):
+  a, b = map(int,input().split())
+  print((a**b) % 10)
