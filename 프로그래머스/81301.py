860066f558b366a5b093330847a258cfececf629
chungchung <61677941+piaochung@users.noreply.github.com>
2022-01-14 02:53:35 +0000

    Feat: "추가 프로그래머스 81301 숫자 문자열과 영단어 풀이"

--- 프로그래머스/81301.py
+++ 프로그래머스/81301.py
@@ -9,9 +9,11 @@ dictionary
 5. 4가 참이라면 dictionary의 value 값을 answer에 더해준다.
 6. 4가 거짓이라면 1을 반복한다.
 """
+number_dict = {"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
+
+# 0.2ms ~ 0.4ms
 def solution(s):
   answer = ""
-  number_dict = {"zero": "0", "one": "1", "two": "2", "three": "3", "four": "4", "five": "5", "six": "6", "seven": "7", "eight": "8", "nine": "9"}
 
   temp = ""
   for char in s:
@@ -26,6 +28,12 @@ def solution(s):
       temp = ""
   return int(answer)
 
+# 0.2ms
+def solution_best(s):
+    answer = s
+    for key, value in number_dict.items():
+        answer = answer.replace(key, value)
+    return int(answer)
 
 s = "one4seveneight"
 print(solution(s))
