65d370954c6fa7f4df9af02cb5fba1e0254dc31c
chungchung <61677941+piaochung@users.noreply.github.com>
2022-01-23 09:10:43 +0000

    Feat: "추가 백준 18870 좌표 압축"

--- main.py
+++ main.py
@@ -1,25 +1,13 @@
-# 백준 No.1620 나는야 포켓몬 마스터 이다솜
-"""
-해쉬
-1. 포켓몬의 이름을 key로 순서는 value로 저장하는 pokemon_dict
-2. 포켓몬 순서를 value로 이름을 key로 저장하는 number_dict
-를 각각 생성하여 검색해준다. dict의 검색 시간복잡도 O(1)
-"""
+# 백준 No.18870 좌표 압축
 
-def solution(target,pokemon_dict, number_dict):
-  if target.isdigit():
-    print(number_dict[int(target)])
-  else:
-    print(pokemon_dict[target])
+def solution(n, number_list):
+  answer_dict = {}
+  sorted_list = sorted(set(number_list))
 
-n, m = map(int, input().split())
+  answer_dict = {sorted_list[i]: i for i in range(len(sorted_list))}
+  for number in number_list:
+    print(answer_dict[number], end=" ")
 
-pokemon_dict, number_dict = {}, {}
-for i in range(n):
-  name = input()
-  pokemon_dict[name] = i+1
-  number_dict[i+1] = name
-
-for _ in range(m):
-  target = input()
-  solution(target,pokemon_dict, number_dict)
+n = int(input())
+number_list = list(map(int, input().split()))
+solution(n, number_list)
