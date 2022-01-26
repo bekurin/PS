41322bbceda8c1f364e0a24f0919b635fe58e373
chungchung <61677941+piaochung@users.noreply.github.com>
2022-01-26 03:04:17 +0000

    Feat: "수정 프로그래머스 42888 오픈채팅방"

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-# 백준 No.1764 듣보잡
+# 백준 No.1931 회의실 배정
+"""
+시작 시간과 끝나는 시간이 같은 회의가 존재하기 때문에 다음과 같이 정렬을 실행한다.
+1. 시작 시간을 기준으로 정렬을 실행한다.
+2. 종료 시간을 기준으로 정렬을 실행한다.
+schedule을 순환하며 시작시간이 마지막 종료 시간보다 크거나 같다면 정답을 늘려주고, 마지막 종료 시간을 초기화해준다.
+"""
+def solution(schedule):
+  answer = 0
+  schedule = sorted(schedule, key=lambda x: x[0])
+  print(schedule)
+  schedule = sorted(schedule, key=lambda x: x[1])
+  print(schedule)
 
-answer = ['c','b','a']
-n, m = map(int, input().split())
-heard, seen = {}, {}
+  last_end = 0
+  for start, end in schedule:
+    if start >= last_end:
+      answer += 1
+      last_end = end 
+  return answer
 
-for _ in range(n):
-  name = input()
-  heard[name] = 1
+# n = int(input())
+# schedule = []
+# for _ in range(n):
+#   schedule.append(list(map(int, input().split())))
+# print(len(solution(schedule)))
 
-for _ in range(m):
-  name = input()
-  if name in heard:
-    answer.append(name)
-
-print(len(answer))
-for item in sorted(answer):
-  print(item)
+schedule = [
+  [1,4],
+  [3,5],
+  [5,7],
+  [6,10],
+  [6,6],
+  [8,11],
+  [2,13],
+  [12,14]
+]
+print(solution(schedule))
