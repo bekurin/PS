aa71fb962f490467c4eafa7cc037d60e6815a4cd
chungchung <61677941+piaochung@users.noreply.github.com>
2022-02-04 12:03:44 +0000

    Feat: "추가 백준 15651 N과 M(3)"

--- 백준/15651.py
+++ 백준/15651.py
@@ -1,5 +1,8 @@
 # 백준 No.15651 N과 M(3)
-
+"""
+백트래킹, 깊이 우선 탐색
+중복된 수를 뽑아도 되기 때문에 dfs 재귀 함수를 호출 할 때에 number_list의 첫번째부터 탐색을 해준다.
+"""
 def dfs(number_list, numbers):
   if len(numbers) == m:
     for number in numbers:
