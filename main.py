7982b5b7177856d558d037959f56293801b137de
chungchung <61677941+piaochung@users.noreply.github.com>
2022-03-08 15:29:26 +0000

    Feat: "추가 백준 15686 치킨 배달"

--- main.py
+++ main.py
@@ -1,26 +1,72 @@
-# 백준 No.15903 카드 합체 놀이
-"""
-자료 구조, 그리디 알고리즘, 우선순위 큐
+# 백준 No.15686 치킨 배달
 
-힙의 크기가 2 이상일 경우에 pop()을 사용하여 작은 값 2개로 꺼내주어 더한 값을 다시 힙에 넣어준다.
-"""
-import sys
-import heapq
-input = sys.stdin.readline
+import copy
+from collections import deque
 
-n, m = map(int, input().split())
-number_list = list(map(int, input().split()))
+def bfs(x, y):
+  distance = 0
+  queue = deque()
+  visited = [[False] * n for _ in range(n)]
 
-heap = []
-for number in number_list:
-  heapq.heappush(heap, number)
+  visited[x][y] = True
+  queue.append((x, y, 1))
 
-for _ in range(m):
-  if len(heap) >= 2:
-    number_sum = 0
-    for _ in range(2):
-      number_sum += heapq.heappop(heap)
+  dx = [-1, 0, 1, 0]
+  dy = [0, -1, 0, 1]
 
-    for _ in range(2):
-      heapq.heappush(heap, number_sum)
-print(sum(heap))
+  while queue:
+    x, y = queue.popleft()
+
+    for i in range(4):
+      nx = x + dx[i]
+      ny = y + dy[i]
+
+      if 0 <= nx < n and 0 <= ny < n:
+        if not visited[nx][ny]:
+          pass
+
+def combination(index, numbers):
+
+  if len(numbers) == m:
+    test_list.append(numbers[:])
+    return
+
+  for i in range(index, len(chicken_list)):
+    numbers.append(chicken_list[i])
+    combination(i + 1, numbers)
+    numbers.pop()
+
+# n, m = map(int, input().split())
+# visited = [[False] * n for _ in range(n)]
+
+# maps = []
+# for _ in range(n):
+#   maps.append(list(map(int, input().split())))
+
+n, m = 5, 3
+maps = [
+  [0, 0, 1, 2, 2], 
+  [0, 0, 2, 2, 1], 
+  [0, 1, 2, 0, 0],
+  [0, 0, 1, 0, 0], 
+  [0, 0, 0, 0, 2]
+]
+
+chicken_list, test_list = [], []
+for x in range(n):
+  for y in range(n):
+    if maps[x][y] == 2:
+      maps[x][y] = 0
+      chicken_list.append(x * n + y % n)
+
+combination(0, [])
+
+for test in test_list:
+  temp_maps = copy.deepcopy(maps)
+  for position in test:
+    x = position // n
+    y = position % n
+
+    temp_maps[x][y] = 2
+  print(temp_maps)
+  
