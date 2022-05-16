01eb9b12acf1163d874aeca49cdd393df0c4d7e4
chungchung <61677941+piaochung@users.noreply.github.com>
2022-05-16 05:15:17 +0000

    Feat: "추가 프로그래머스 42627 디스크 컨트롤러"

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
-# 프로그래머스 No.42579 베스트앨범
-def get_music_dict(genres, plays):
-  music_dict = {}
-  for idx, item in enumerate(list(zip(genres, plays))):
-    if item[0] not in music_dict:
-      music_dict[item[0]] = [(idx, item[1])]
-    else:
-      music_dict[item[0]].append((idx, item[1]))
-  return music_dict
+# 프로그래머스 No.42627 디스크 컨트롤러
+from queue import PriorityQueue
 
-def get_best_genre(music_dict):
-  best_genre = []
-  for key, value in music_dict.items():
-    temp = 0
-    for item in value:
-      temp += item[-1]
-    best_genre.append((key, temp))
-  return sorted(best_genre, key = lambda x: x[-1], reverse = True)
-
-def solution(genres, plays):
-  answer = []
-  music_dict = get_music_dict(genres, plays)
-  best_genre = get_best_genre(music_dict)
-
-  for key, value in music_dict.items():
-    music_dict[key] = sorted(value, key = lambda x: x[-1], reverse = True)
+def SJFS(jobs):
+  start, task = 0, 0
+  time, answer = 0, 0
     
-  for genre, count in best_genre:
-    if len(music_dict[genre]) == 1:
-      answer.append(music_dict[genre][0][0])
+  ready_queue = PriorityQueue()
+  for start, task in jobs:
+    if time >= start:
+      print("[1] put ready_queue task = {}, start = {}".format(task, start))
+      ready_queue.put((task, start))
     else:
-      for item in music_dict[genre][:2]:
-        answer.append(item[0])
-  return answer
+      # not proccessed start, task is remain
+      while start >= time and not ready_queue.empty():
+        ready_task, ready_start = ready_queue.get()
+        print("[2] get ready_queue task = {}, start = {}".format(ready_task, ready_start))
+        
+        time += ready_task
+        answer += abs(time - ready_start)
+        print("[3] change time = {}, answer = {}".format(time, answer))
+        
+      if not ready_queue.empty():
+        if ready_start < time:
+          time += ready_task
+          answer += abs(time - ready_start)
+          print("[4] change time = {}, answer = {}".format(time, answer))
+      else:
+          time = ready_task + ready_start
+          answer += ready_task
+          print("[5] change time = {}, answer = {}".format(time, answer))
+  return answer + task
 
-genres = ["classic", "pop", "classic", "classic", "pop"]
-plays = [500, 600, 150, 800, 2500]
-print(solution(genres, plays))
+def solution(jobs):
+  sorted_jobs = sorted(jobs, key = lambda x: (x[0], x[1]))
+  return SJFS(sorted_jobs)
+
+jobs = [[0, 3], [1, 9], [2, 6]]
+print(solution(jobs))
