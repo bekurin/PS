67cdf104c2bd1726f63289c8f499326f2f541006
chungchung <61677941+piaochung@users.noreply.github.com>
2022-05-20 06:27:10 +0000

    Feat: "추가 프로그래머스 67258 보석 쇼핑"

--- main.py
+++ main.py
@@ -1,65 +1,32 @@
-# 프로그래머스 No.64064 불량 사용자
-def match_pattern(user, banned):
-  if len(user) != len(banned):
-    return False
+# 프로그래머스 No.67258 [카카오 인턴] 보석 쇼핑
+def get_dict_and_count(gems):
+  gem_dict = {}
+  for item in set(gems):
+    gem_dict[item] = 0
+  return gem_dict, len(gem_dict.keys())
 
-  for i in range(len(user)):
-    if banned[i] == '*': continue
-    elif user[i] != banned[i]: return False
-  return True
+def is_minimum(right, left, current):
+  if right - left < current[1] - current[0]:
+    return True
+  return False
 
-def get_candidate_list(user_id, banned_id):
-  candidate_list = []
-  for banned in banned_id:
-    candidates = []
-    for user in user_id:
-      if match_pattern(user, banned):
-        candidates.append(user)
-    candidate_list.append(candidates)
-  return candidate_list
+def solution(gems):
+  answer = [0, len(gems)]
+  gem_dict, count = get_dict_and_count(gems)
+  left, right = 0, 0
 
-def bfs(candidate_list, target, current, index):
-  global case_list
-  if len(current) == target:
-    case_list.append(current[:])
-    return
+  while left <= right and right < len(gems):
+    if 0 not in gem_dict.values():
+      gem_dict[gems[left]] -= 1
+      left += 1
+      if is_minimum(right, left, answer):
+        answer = [left, right]
+    elif right < len(gems):
+      gem_dict[gems[right]] += 1
+      right += 1
+    else:
+      return answer
+  return answer
 
-  for i in range(index, len(candidate_list)):
-    for candidate in candidate_list[i]:
-      if candidate not in current:
-        current.append(candidate)
-        bfs(candidate_list, target, current, i + 1)
-        current.pop()
-
-def is_avaiable(answer, case):
-  for item_list in answer:
-    count = 0
-    for item in item_list:
-      if item in case: count += 1
-    if count == len(case): return False
-  return True
-        
-case_list = []
-def solution(user_id, banned_id):
-  global case_list
-  case_list = []
-  
-  candidate_list = get_candidate_list(user_id, banned_id)
-  bfs(candidate_list, len(banned_id), [], 0)
-
-  answer = []
-  for case in case_list:
-    if not answer:
-      answer.append(case)
-    elif is_avaiable(answer, case):
-      answer.append(case)    
-  return len(answer)
-
-user_id = ["frodo", "fradi", "crodo", "abc123", "frodoc"]
-banned_id_list = [
-  ["fr*d*", "abc1**"],
-  ["*rodo", "*rodo", "******"],
-  ["fr*d*", "*rodo", "******", "******"]
-]
-for banned_id in banned_id_list:
-  print(solution(user_id, banned_id))
+gems = 	["AA", "AB", "AC", "AA", "AC"]
+print(solution(gems))
