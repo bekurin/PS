d0022ec6aad38c957b7619a5963013d188893d73
chungchung <61677941+piaochung@users.noreply.github.com>
2022-07-29 16:41:45 +0900

    Feat: "수정 선택 정렬 알고리즘 구현 코드"

--- 알고리즘 정의/정렬 알고리즘/selection_sort.py
+++ 알고리즘 정의/정렬 알고리즘/selection_sort.py
@@ -2,7 +2,7 @@ import random
 
 
 def selection_sort(array):
-    for i in range(len(array)):
+    for i in range(len(array) - 1):
         min_index = i
         for j in range(i + 1, len(array)):
             if array[j] < array[min_index]:
