40a122df1d5adfa77aa28adb8c1bc70d3ce748a5
chungchung <61677941+piaochung@users.noreply.github.com>
2022-08-28 20:21:20 +0900

    Feat: "수정 프로그래머스 17681 [1차] 비밀지도"

--- 프로그래머스/17681.py
+++ 프로그래머스/17681.py
@@ -1,50 +1,37 @@
 # 프로그래머스 No.17681 [1차] 비밀지도
-"""
-배열, 진법 계산
-1. arr1을 순환하며 이진법 배열로 바꿔준다. 이때, n에 맞게 자릿수를 채워준다.
-2. arr2를 순환하며 이진법 배열로 바꿔준다. 이때, n에 맞게 자릿수를 채워준다.
-3. 이중반복문을 사용하여 모든 이차원 배열의 값을 순환한다.
-4. 3의 과정 중에 벽을 발견하면 temp에 "#"을 더해준다.
-5. 3의 과정 중에 벽을 발견하지 못하면 temp에 " "을 더해준다.
-6. answer에 temp를 추가해준다.
-7. 3을 반복한다.
-"""
+
 def get_binary_list(number, n):
-  base = ''
-  print(number)
-  while number > 0:
-    number, mode = divmod(number,2)
-    base += str(mode)
+    base = ''
+    while number > 0:
+        number, mode = divmod(number, 2)
+        base += str(mode)
 
-  if len(base) < n:
-    while len(base) < n:
-      base += '0'
+    if len(base) < n:
+        base += '0' * (n - len(base))
+    return list(base[::-1])
 
-  base = base[::-1]
-  return list(map(int, base))
 
 def solution(n, arr1, arr2):
-  answer = []
-  map1, map2 = [], []
+    answer = []
+    arr1_map_list = get_map_list(n, arr1)
+    arr2_map_list = get_map_list(n, arr2)
 
-  for num in arr1:
-    map1.append(get_binary_list(num, n))
-  for num in arr2:
-    map2.append(get_binary_list(num, n))
+    for x in range(n):
+        temp = ''
+        for y in range(n):
+            temp += '#' if arr1_map_list[x][y] or arr2_map_list[x][y] else ' '
+        answer.append(temp)
+    return answer
 
-  print(map1)
-  print(map2)
-  for i in range(n):
-    temp = ""
-    for j in range(n):
-      if map1[i][j] == 1 or map2[i][j] == 1:
-        temp += "#"
-      else:
-        temp += " "
-    answer.append(temp)
-  return answer
 
-n = 6
-arr1 = 	[46, 33, 33 ,22, 31, 50]
-arr2 = 	[27 ,56, 19, 14, 14, 10]
+def get_map_list(n, array):
+    map_list = []
+    for number in array:
+        map_list.append(list(map(int, get_binary_list(number, n))))
+    return map_list
+
+
+n = 5
+arr1 = [9, 20, 28, 18, 11]
+arr2 = [30, 1, 21, 17, 28]
 print(solution(n, arr1, arr2))
