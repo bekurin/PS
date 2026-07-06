bb5befcf197dc03f0c5e625015b772e72d7116c1
chungchung <hangjastar@gmail.com>
2026-07-06 23:04:21 +0900

    feat: implement groupAnagrams

---
+++ 리트코드/group_anagrams.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagram_map = defaultdict(list)
+
+        for s in strs:
+            sorted_s = "".join(sorted(s))
+            anagram_map[sorted_s].append(s)
+
+        return list(anagram_map.values())
